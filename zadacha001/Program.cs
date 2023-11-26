a52d064be596ba41db0c524bd1e6f26bdb808c51
Vlad Dre <dreier.vlad@mail.ru>
2023-11-26 18:32:51 +0300

    Задача 1

---
+++ zadacha001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+class Programm{
+
+  static void  Main(){
+     Console.WriteLine("enter number");
+     int number = Convert.ToInt32(Console.ReadLine());
+      if(number % 7 ==0 && number % 23 == 0){
+         Console.WriteLine($"Число {number} кратно числу 7 и числу 23");
+        }
+      else{
+          Console.WriteLine($"Число {number} не кратно числу 7 и числу 23");
+      }  
+  }
+ 
+ 
+}
+
