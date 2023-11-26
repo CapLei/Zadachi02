9adde4372813db78bfc3e63f59185590b70956dc
Vlad Dre <dreier.vlad@mail.ru>
2023-11-26 19:21:58 +0300

    Задача 3

---
+++ zadacha003/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Programm{
+    static void Main(){
+     Console.WriteLine("Введите число из отрезка [10;23]:");
+     int number = Convert.ToInt32(Console.ReadLine());
+
+
+     int a = number / 10;
+     int b = number % 10;
+     int max = a;
+    
+     if (max < b){
+     max = b;
+     }
+     else{
+        max = a;
+     }
+        Console.WriteLine(max);
+    }
+
+    
+
+}
