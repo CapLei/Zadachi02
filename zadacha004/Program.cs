adfb13e7a698181bcd8fe8367b8b643e0c40fbd4
Vlad Dre <dreier.vlad@mail.ru>
2023-11-26 19:37:43 +0300

    Задача 4

---
+++ zadacha004/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Programm{
+    static void Main(){
+        Console.Write("Введите натуральное число N ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        if(N <0 ){
+            Console.WriteLine(N);
+        }
+        else{
+            while(N > 0){
+                int count = N % 10;
+                N /= 10;
+                if(N > 0){
+                    Console.Write(count + ",");
+                }
+                else{
+                    Console.WriteLine(count);
+                }
+            }
+        }
+    }
+
+
+}
